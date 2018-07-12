65545aa24bc2e7df73709d73cf2d23b39de82fb5
Adri <forczekadri@gmail.com>
2018-07-12 12:14:50 +0100

    adds basic class structure

---
+++ src/main/java/models/Restaurant.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.List;
+
+public class Restaurant {
+
+    private int id;
+    private String name;
+    private List<Table> tables;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+}
